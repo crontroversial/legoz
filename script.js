656b9fcadddc35af0d6af47b602dc48d2d7e6301
crontroversial <nadine.geiger98@icloud.com>
2026-09-09 16:50:17 +0200

    Add button click event to show alert

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("JavaScript läuft!");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    alert("Du hast Lego gekauft!");
+  });
+});
