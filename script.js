14c009bcecf6df340600c4a166ba654e3a8db984
crontroversial <nadine.geiger98@icloud.com>
2026-09-09 16:53:25 +0200

    Update alert message to include product name

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const buttons = document.querySelectorAll("button");
 
 buttons.forEach(function(button) {
   button.addEventListener("click", function() {
-    alert("Du hast Lego gekauft!");
+
+    const product = button.closest(".product");
+    const productName = product.querySelector("h2").textContent;
+
+    alert(productName + " wurde hinzugefügt!");
+
   });
 });
