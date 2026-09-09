d3344b76e0edf777f111a8afe69fc6eadaefaeb2
crontroversial <nadine.geiger98@icloud.com>
2026-09-09 16:58:24 +0200

    Add cart count functionality to button clicks

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const buttons = document.querySelectorAll("button");
+const cartCount = document.querySelector("#cart-count");
+
+let count = 0;
 
 buttons.forEach(function(button) {
   button.addEventListener("click", function() {
 
     const product = button.closest(".product");
     const productName = product.querySelector("h2").textContent;
+
+    count = count + 1;
+    cartCount.textContent = count;
 
     alert(productName + " wurde hinzugefügt!");
 
